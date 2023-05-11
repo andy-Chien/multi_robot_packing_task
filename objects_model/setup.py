8b061a48ff88cc6838213b72e569d0e24d9b9fcf
Andy Chien <r960411@gmail.com>
2023-05-11 22:04:46 +0800

    add packing box

--- objects_model/setup.py
+++ objects_model/setup.py
@@ -16,6 +16,7 @@ setup(
         (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
         (os.path.join('share', package_name, 'mesh'), glob('mesh/*.obj')),
         (os.path.join('share', package_name, 'urdf'), glob('urdf/*.urdf')),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.stl')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
